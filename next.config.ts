ec568c39aad7edaf22a889e146373035faf8e423
Leunhart <jessicajeffry991@gmail.com>
2026-01-04 03:08:37 +0700

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     domains: ['example.com', 'res.cloudinary.com', 'images.unsplash.com'],
   },
